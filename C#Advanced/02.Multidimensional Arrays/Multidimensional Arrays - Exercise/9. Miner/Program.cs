c77e182fb001139b9b3777e24df682b230f951e3
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-01-03 20:43:38 +0200

    exercises

--- C#Advanced/02.Multidimensional Arrays/Multidimensional Arrays - Exercise/9. Miner/Program.cs
+++ C#Advanced/02.Multidimensional Arrays/Multidimensional Arrays - Exercise/9. Miner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _9._Miner
 {
@@ -6,9 +7,133 @@ namespace _9._Miner
     {
         static void Main(string[] args)
         {
+            int size = int.Parse(Console.ReadLine());
+            string[] commands = Console
+                          .ReadLine()
+                          .Split(" ")
+                          .ToArray();
 
+            char[,] matrix = new char[size, size];
+            ReadInput(matrix);
 
+            int Srow = 0;
+            int Scol = 0;
+            FindStart(matrix, ref Srow, ref Scol);
 
+            bool IsInRange = false;
+
+            int valueOfCoals = matrix.Cast<char>().Where(x => x == 'c').Count();
+
+            int currentValueOfCoals = 0;
+            int rowIndex = 0;
+            int colIndex = 0;
+
+            char letter = ' ';
+            foreach (var command in commands)
+            {
+                switch (command)
+                {
+                    case "up":
+                        if (IsInside(matrix, Srow - 1, Scol))
+                        {
+                            IsInRange = true;
+                            letter = matrix[Srow - 1, Scol];
+                            rowIndex = Srow - 1;
+                            colIndex = Scol;
+                            Srow -= 1;
+
+                        }
+                        break;
+                    case "down":
+                        if (IsInside(matrix, Srow + 1, Scol))
+                        {
+                            IsInRange = true;
+                            letter = matrix[Srow + 1, Scol];
+                            rowIndex = Srow + 1;
+                            colIndex = Scol;
+                            Srow += 1;
+                        }
+                        break;
+                    case "right":
+                        if (IsInside(matrix, Srow, Scol + 1))
+                        {
+                            IsInRange = true;
+                            letter = matrix[Srow, Scol + 1];
+                            rowIndex = Srow;
+                            colIndex = Scol + 1;
+                            Scol += 1;
+                        }
+                        break;
+                    case "left":
+                        if (IsInside(matrix, Srow, Scol - 1))
+                        {
+                            IsInRange = true;
+                            letter = matrix[Srow, Scol - 1];
+                            rowIndex = Srow;
+                            colIndex = Scol - 1;
+                            Scol -= 1;
+                        }
+                        break;
+                }
+                if (IsInRange)
+                {
+                    if (letter == 'c')
+                    {
+                        currentValueOfCoals++;
+                        matrix[rowIndex, colIndex] = '*';
+                        if (valueOfCoals == currentValueOfCoals)
+                        {
+                            Console.WriteLine($"You collected all coals!" +
+                                $" ({rowIndex}, {colIndex})");
+                            return;
+                        }
+                        continue;
+                    }
+                    if (letter == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({rowIndex}, {colIndex})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{valueOfCoals - currentValueOfCoals} coals left. ({rowIndex}, {colIndex})");
+        }
+        private static void FindStart(char[,] matrix, ref int Srow, ref int Scol)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 's')
+                    {
+                        Srow = row;
+                        Scol = col;
+                    }
+                }
+            }
+        }
+
+        private static void ReadInput(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console
+                          .ReadLine()
+                          .Split(" ")
+                          .Select(char.Parse)
+                          .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+
+        private static bool IsInside(char[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0)
+               && col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
---
+++ C#Advanced/02.Multidimensional Arrays/Multidimensional Arrays - Exercise/last exercise/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Linq;
+
+namespace last_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console
+                          .ReadLine()
+                          .Split(" ")
+                          .Select(int.Parse)
+                          .ToArray();
+
+            char[,] matrix = new char[dimensions[0], dimensions[1]];
+            ReadInput(matrix);
+
+            char[] commands = Console
+                           .ReadLine()
+                           .ToCharArray();
+            int Prow = 0;
+            int Pcol = 0;
+            FindSartPosition(matrix, ref Prow, ref Pcol);
+
+            bool isInside = false;
+            bool isDied = false;
+            int diedRow = 0;
+            int diedCol = 0;
+
+            int wonRow = 0;
+            int wondCol = 0;
+            foreach (var command in commands)
+            {
+                if (command == 'U')
+                {
+                    if (IsInside(matrix, Prow - 1, Pcol))
+                    {
+                        if (matrix[Prow - 1, Pcol] == 'B')
+                        {
+                            isDied = true;
+                        }
+                        matrix[Prow, Pcol] = '.';
+                        matrix[Prow - 1, Pcol] = 'P';
+                        isInside = true;
+                        Prow--;
+                    }
+                    else
+                    {
+                        wonRow = Prow ;
+                        wondCol = Pcol;
+                    }
+                }
+                else if (command == 'L')
+                {
+                    if (IsInside(matrix, Prow, Pcol - 1))
+                    {
+                        if (matrix[Prow, Pcol - 1] == 'B')
+                        {
+                            isDied = true;
+                        }
+                        matrix[Prow, Pcol] = '.';
+                        matrix[Prow, Pcol - 1] = 'P';
+                        isInside = true;
+                        Pcol--;
+                    }
+                    else
+                    {
+                        wonRow = Prow;
+                        wondCol = Pcol;
+                    }
+                }
+                else if (command == 'R')
+                {
+                    if (IsInside(matrix, Prow, Pcol + 1))
+                    {
+                        if (matrix[Prow, Pcol + 1] == 'B')
+                        {
+                            isDied = true;
+                        }
+                        matrix[Prow, Pcol] = '.';
+                        matrix[Prow, Pcol + 1] = 'P';
+                        isInside = true;
+                        Pcol++;
+                    }
+                    else
+                    {
+                        wonRow = Prow;
+                        wondCol = Pcol ;
+                    }
+                }
+                else if (command == 'D')
+                {
+                    if (IsInside(matrix, Prow + 1, Pcol))
+                    {
+                        if (matrix[Prow + 1, Pcol] == 'B')
+                        {
+                            isDied = true;
+                        }
+                        matrix[Prow, Pcol] = '.';
+                        matrix[Prow + 1, Pcol] = 'P';
+                        isInside = true;
+                        Prow++;
+                    }
+                    else
+                    {
+                        wonRow = Prow ;
+                        wondCol = Pcol;
+                    }
+                }
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (matrix[row, col] == 'B')
+                        {
+                            if (IsInside(matrix, row - 1, col))
+                            {
+                                if (matrix[row - 1, col] == 'P')
+                                {
+                                    isDied = true;
+                                    diedRow = row;
+                                    diedCol = col;
+                                }
+                                matrix[row - 1, col] = 'B';
+                            }
+                            if (IsInside(matrix, row +1, col))
+                            {
+                                if (matrix[row + 1, col] == 'P')
+                                {
+                                    isDied = true;
+                                    diedRow = row;
+                                    diedCol = col;
+                                }
+                                matrix[row + 1, col] = 'B';
+                            }
+                            if (IsInside(matrix, row, col - 1))
+                            {
+                                if (matrix[row , col-1] == 'P')
+                                {
+                                    isDied = true;
+                                    diedRow = row;
+                                    diedCol = col;
+                                }
+                                matrix[row, col - 1] = 'B';
+                            }
+                            if (IsInside(matrix, row, col + 1))
+                            {
+                                if (matrix[row, col+1] == 'P')
+                                {
+                                    isDied = true;
+                                    diedRow = row;
+                                    diedCol = col;
+                                }
+                                matrix[row, col + 1] = 'B';
+                            }
+                        }
+                    }
+                }
+                if (isDied)
+                {
+                    break;
+                }
+                if (!isInside)
+                {
+                    break;
+                }
+                //?
+                //TO DO...
+            }
+            ;
+            PrintMatrix(matrix);
+            if (!isDied)
+            {
+                Console.WriteLine($"won: {wonRow} {wondCol}");
+            }
+            else
+            {
+                Console.WriteLine($"dead: {diedRow} {diedCol}");
+            }
+        }
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FindSartPosition(char[,] matrix, ref int Prow, ref int Pcol)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'P')
+                    {
+                        Prow = row;
+                        Pcol = col;
+                    }
+                }
+            }
+        }
+
+        private static void ReadInput(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console
+                          .ReadLine()
+                          .ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+        private static bool IsInside(char[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0)
+               && col >= 0 && col < matrix.GetLength(1);
+        }
+
+    }
+}
