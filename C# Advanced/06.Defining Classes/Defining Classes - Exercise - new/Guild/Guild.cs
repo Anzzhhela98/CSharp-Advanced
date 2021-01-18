8ece882f0ded255064751133a35a558d7ff7659e
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-01-18 21:15:21 +0200

    Task Table

---
+++ C# Advanced/06.Defining Classes/Defining Classes - Exercise - new/Guild/Guild.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        public Guild(string nameGuild, int capacity)
+        {
+            NameGuild = nameGuild;
+            Capacity = capacity;
+            roster = new List<Player>();
+        }
+
+        private List<Player> roster;
+
+
+        public string NameGuild { get; set; }
+        public int Capacity { get; set; }
+
+        public void AddPlayer(Player player)
+        {
+            if (roster.Count < this.Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+        public bool RemovePlayer(string namey)
+        {
+            if (roster.Any(x => x.Name == namey))
+            {
+                roster.RemoveAll(x => x.Name == namey);
+                return true;
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        {
+
+            if (roster.Any(x => x.Name == name && x.Rank != "Member"))
+            {
+                roster.Find(x => x.Name == name).Rank = "Member";
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            if (roster.Any(x => x.Name == name && x.Rank != "Trial"))
+            {
+                roster.Find(x => x.Name == name).Rank = "Trial";
+            }
+        }
+        public Player[] KickPlayersByClass(string clas)
+        {
+            Player[] arr = roster.Where(x => x.Class == clas).ToArray();
+            roster = roster.Where(x => x.Class != clas).ToList();
+
+            return arr;
+        }
+        public int Count => this.roster.Count;
+
+        public string Report()
+        {
+
+            StringBuilder rezult = new StringBuilder();
+
+            rezult.AppendLine($"Players in the guild: {this.NameGuild}");
+            foreach (var player in roster)
+            {
+                rezult.AppendLine($"Player {player.Name}: {player.Class}");
+                rezult.AppendLine($"Rank: {player.Rank}");
+                rezult.AppendLine($"Description: {player.Description}");
+            }
+            return rezult.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/06.Defining Classes/Defining Classes - Exercise - new/Guild/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+        private string rank;
+        private string descriptopn;
+        public Player(string name, string @class)
+        {
+            Name = name;
+            Class = @class;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+
+        public string Rank
+        {
+            get { return rank; }
+            set { rank = value; }
+        }
+
+        public string Description
+        {
+            get { return descriptopn; }
+            set { descriptopn = value; }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Player {this.Name}: {this.Class}");
+            sb.AppendLine($"Rank: {this.Rank}");
+            sb.AppendLine($"Description: {this.Description}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
