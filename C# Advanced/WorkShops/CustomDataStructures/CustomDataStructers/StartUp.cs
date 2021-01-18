b3ad256aa177f4f37da622d58bf1524e80f38e61
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-01-18 21:18:45 +0200

    Custom List

---
+++ C# Advanced/WorkShops/CustomDataStructures/CustomDataStructers/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CustomDataStructers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new CustomList();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+
+            }
+            ;
+            //Debug.Assert(list[2] == 9);
+        }
+    }
+}
