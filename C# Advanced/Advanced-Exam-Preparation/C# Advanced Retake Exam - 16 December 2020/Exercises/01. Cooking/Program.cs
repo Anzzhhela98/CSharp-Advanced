073b668cfb245b81f5fa8b0b94b190ff04e28456
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-02-11 22:04:41 +0200

    Preparation

---
+++ C# Advanced/Advanced-Exam-Preparation/C# Advanced Retake Exam - 16 December 2020/Exercises/01. Cooking/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Cooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prodcuts = new Dictionary<string, int>()
+            {
+                ["Pastry"] = 0,
+                ["Bread"] = 0,
+                ["Fruit Pie"] = 0,
+                ["Cake"] = 0,
+
+            };
+
+            Queue<double> liquids;
+            Stack<double> ingredians;
+
+            ReadInput(out liquids, out ingredians);
+
+            while (liquids.Count != 0 && ingredians.Count != 0)
+            {
+                double liquid = liquids.Dequeue();
+                double ingredian = ingredians.Pop();
+                double amount = liquid + ingredian;
+
+                switch (amount)
+                {
+                    case 25:
+                        prodcuts["Bread"] += 1;
+                        break;
+                    case 50:
+                        prodcuts["Cake"] += 1;
+                        break;
+                    case 75:
+                        prodcuts["Fruit Pie"] += 1;
+                        break;
+                    case 100:
+                        prodcuts["Pastry"] += 1;
+                        break;
+                    default:
+                        ingredians.Push(ingredian + 3);
+                        break;
+                }
+            }
+
+            bool isEvrithingCooked = prodcuts.All(x => x.Value > 0);
+
+            Print(prodcuts, liquids, ingredians, isEvrithingCooked);
+        }
+
+        private static void ReadInput(out Queue<double> liquids, out Stack<double> ingredians)
+        {
+            liquids = new Queue<double>
+                (Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse));
+            ingredians = new Stack<double>
+(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse));
+        }
+
+        private static void Print(Dictionary<string, int> prodcuts, Queue<double> liquids, Stack<double> ingredians, bool isEvrithingCooked)
+        {
+            Console.WriteLine(isEvrithingCooked == true ? "Wohoo! You succeeded in cooking all the food!" : "Ugh, what a pity! You didn't have enough materials to cook everything.");
+
+            Console.WriteLine(liquids.Count == 0 ? "Liquids left: none" : $"Liquids left: {string.Join(", ", liquids)}");
+
+            Console.WriteLine(ingredians.Count == 0 ? "Ingredients left: none" : $"Ingredients left: {string.Join(", ", ingredians)}");
+
+            foreach (var product in prodcuts.OrderBy(p => p.Key))
+            {
+                Console.WriteLine($"{product.Key}: {product.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Advanced-Exam-Preparation/C# Advanced Retake Exam - 16 December 2020/Exercises/Selling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCol = int.Parse(Console.ReadLine());
+            FillMatrix(rowCol);
+
+        }
+
+        private static void FillMatrix(int rowCol)
+        {
+            char[,] matrix = new char[rowCol, rowCol];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                   
+                }
+            }
+        }
+    }
+}
