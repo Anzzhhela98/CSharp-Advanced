f594196fd87b732fe79cc6db52ed1c492d08641f
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-02-12 23:54:36 +0200

    Preparation

---
+++ C# Advanced/Advanced-Exam-Preparation/C# Advanced Exam - 25 October 2020/Exercises/01. Scheduling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var thread = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int numberToKill = int.Parse(Console.ReadLine());
+
+            bool isKilled = false;
+
+            while (!isKilled)
+            {
+
+                int currentTask = task.Peek();
+                int currentTread = thread.Peek();
+
+                if (currentTask == numberToKill)
+                {
+                    Console.WriteLine($"Thread with value {currentTread} killed task {numberToKill}");
+                    isKilled = true;
+                    break;
+                }
+                else if (currentTread >= currentTask)
+                {
+                    task.Pop();
+                }
+
+                thread.Dequeue();
+            }
+            Console.WriteLine(String.Join(" ", thread));
+        }
+    }
+}
---
+++ C# Advanced/Advanced-Exam-Preparation/C# Advanced Exam - 25 October 2020/Exercises/02. Garden/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console
+                              .ReadLine()
+                              .Split()
+                              .Select(int.Parse)
+                              .ToArray();
+
+            int[,] matrix = new int[data[0], data[1]];
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] flowerPosition = command
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+                int currentRow = flowerPosition[0];
+                int currentCol = flowerPosition[0];
+
+                if (Validate(matrix, currentRow, currentCol))
+                {
+                    for (int row = currentRow; row < currentRow + 1; row++)
+                    {
+                        for (int col = 0; col < matrix.GetLength(1); col++)
+                        {
+                            matrix[row, col] += 1;
+
+                        }
+                    }
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        for (int col = currentCol; col < currentCol+1; col++)
+                        {
+                            if (row==currentRow&&col==currentCol)
+                            {
+                                continue;
+                            }
+                            matrix[row, col] += 1;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool Validate(int[,] matrix, int currentRow, int currentCol)
+        {
+            if (currentRow >= 0 && currentRow < matrix.GetLength(0) && currentCol >= 0 && currentCol < matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
