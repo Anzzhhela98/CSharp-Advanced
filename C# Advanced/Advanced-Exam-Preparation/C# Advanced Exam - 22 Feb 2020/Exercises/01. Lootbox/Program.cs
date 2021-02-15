558721b679041f511fefc25f646301c0d366addc
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-02-15 11:49:36 +0200

    Preparation Exam

--- C# Advanced/Advanced-Exam-Preparation/C# Advanced Exam - 22 Feb 2020/Exercises/01. Lootbox/Program.cs
+++ C# Advanced/Advanced-Exam-Preparation/C# Advanced Exam - 22 Feb 2020/Exercises/01. Lootbox/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Lootbox
 {
@@ -6,7 +8,49 @@ namespace _01._Lootbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int NEEDED_CLAIMD_ITEMS = 100;
+
+            var firstBox = new Queue<int>
+                (Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var secondBox = new Stack<int>
+                (Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int claimedItems = 0;
+
+            while (firstBox.Count > 0 && secondBox.Count > 0)
+            {
+                int firtsItem = firstBox.Peek();
+                int secondItem = secondBox.Pop();
+
+                int sum = firtsItem + secondItem;
+
+                if (sum % 2 == 0)
+                {
+                    claimedItems += sum;
+                    firstBox.Dequeue();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondItem);
+                }
+            }
+            if (firstBox.Count==0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            if (claimedItems>=NEEDED_CLAIMD_ITEMS)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimedItems}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimedItems}");
+            }
+
         }
     }
 }
--- C# Advanced/Advanced-Exam-Preparation/C# Advanced Exam - 22 Feb 2020/Exercises/02. Re-Volt/Program.cs
+++ C# Advanced/Advanced-Exam-Preparation/C# Advanced Exam - 22 Feb 2020/Exercises/02. Re-Volt/Program.cs
@@ -6,7 +6,195 @@ namespace _02._Re_Volt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+
+            int countOfMovement = int.Parse(Console.ReadLine());
+            int startRow = -1;
+            int startCol = -1;
+
+            InitialiseMatrix(matrix, ref startRow, ref startCol);
+
+            bool isReachedFinishMark = false;
+            for (int i = 0; i < countOfMovement; i++)
+            {
+                string movement = Console.ReadLine();
+
+                if (movement == "right")
+                {
+                    matrix[startRow, startCol] = '-';
+                    startCol += 1;
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startCol = 0;
+                    }
+                    else if (matrix[startRow, startCol] == 'B')
+                    {
+                        startCol += 1;
+
+                    }
+                    else if (matrix[startRow, startCol] == 'T')
+                    {
+                        startCol -= 1;
+
+                    }
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startCol = size - 1;
+                    }
+                    if (matrix[startRow, startCol] == 'F')
+                    {
+                        isReachedFinishMark = true;
+                        matrix[startRow, startCol] = 'f';
+                        break;
+                    }
+                    matrix[startRow, startCol] = 'f';
+                }
+                else if (movement == "left")
+                {
+                    matrix[startRow, startCol] = '-';
+                    startCol -= 1;
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startCol = size - 1;
+                    }
+                    else if (matrix[startRow, startCol] == 'B')
+                    {
+                        startCol -= 1;
+
+                    }
+                    else if (matrix[startRow, startCol] == 'T')
+                    {
+
+                        startCol += 1;
+
+                    }
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startCol = size - 1;
+                    }
+                    if (matrix[startRow, startCol] == 'F')
+                    {
+                        isReachedFinishMark = true;
+                        matrix[startRow, startCol] = 'f';
+                        break;
+                    }
+                    matrix[startRow, startCol] = 'f';
+                }
+                else if (movement == "up")
+                {
+                    matrix[startRow, startCol] = '-';
+                    startRow -= 1;
+
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startRow = size - 1;
+                    }
+                    else if (matrix[startRow, startCol] == 'B')
+                    {
+                        startRow -= 1;
+
+                    }
+                    else if (matrix[startRow, startCol] == 'T')
+                    {
+                        startRow += 1;
+
+                    }
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startCol = size - 1;
+                    }
+                    if (matrix[startRow, startCol] == 'F')
+                    {
+                        isReachedFinishMark = true;
+                        matrix[startRow, startCol] = 'f';
+                        break;
+                    }
+                    matrix[startRow, startCol] = 'f';
+                }
+                else if (movement == "down")
+                {
+                    matrix[startRow, startCol] = '-';
+                    startRow += 1;
+
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startRow = 0;
+                    }
+                    else if (matrix[startRow, startCol] == 'B')
+                    {
+                        startRow += 1;
+
+                    }
+                    else if (matrix[startRow, startCol] == 'T')
+                    {
+                        startRow -= 1;
+
+                    }
+                    if (!Validate(matrix, startRow, startCol))
+                    {
+                        startCol = size - 1;
+                    }
+                    if (matrix[startRow, startCol] == 'F')
+                    {
+                        isReachedFinishMark = true;
+                        matrix[startRow, startCol] = 'f';
+                        break;
+                    }
+                    matrix[startRow, startCol] = 'f';
+                }
+            }
+            if (isReachedFinishMark)
+            {
+                Console.WriteLine("Player won!");
+            }
+            else
+            {
+                Console.WriteLine("Player lost!");
+            }
+            PrintMatrix(matrix);
+        }
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool Validate(char[,] matrix, int currentRow, int currentCol)
+        {
+            if (currentRow >= 0 && currentRow < matrix.GetLength(0) && currentCol >= 0 && currentCol < matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+        private static void InitialiseMatrix(char[,] matrix, ref int startRow, ref int startCol)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string data = Console.ReadLine();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = data[col];
+                    FindStartPosition(matrix, ref startRow, ref startCol, row, col);
+                }
+            }
+        }
+
+        private static void FindStartPosition(char[,] matrix, ref int startRow, ref int startCol, int row, int col)
+        {
+            if (matrix[row, col] == 'f')
+            {
+                startRow = row;
+                startCol = col;
+            }
         }
     }
 }
