3c9435e28dadf894692235cb00ff7eacec0dcec5
Anzhela Nurieva <anzzhhela98@gmail.com>
2021-03-05 23:22:56 +0200

    UnitTests

---
+++ C# OOP/07.Unit Testing/Unit Testing - Exercises/CarManager.Tests/CarTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CarTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
